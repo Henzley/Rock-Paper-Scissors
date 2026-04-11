78d7070096d9aafbe7df05ad663e4cdcb3fe83ba
Henzley Spogter <spoggie.hs@gmail.com>
2026-04-11 22:36:53 +0200

    Create function that prompts human to input rock, paper, or scissors. And return human input.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let humanScore = 0;
 let computerScore = 0;
 
+//Write a function that gets the computer's choice
 function getComputerChoice() {
   //Getting a random number between 1 and 3 to assign rock, paper, or scissors
   let randomNum = Math.floor(Math.random() * (3 - 1) + 1);
@@ -11,4 +12,12 @@ function getComputerChoice() {
   } else {
     return "scissors";
   }
+}
+
+//Write a function that gets the human's choice
+function getHumanChoice() {
+  let humanChoice = prompt(
+    "Please enter rock, paper, or scissors:",
+  ).toLowerCase();
+  return humanChoice;
 }
