c68e71148ddb14f96d760cc3ad2ee872633751aa
Henzley Spogter <spoggie.hs@gmail.com>
2026-04-11 22:06:19 +0200

    Create working function to get the computer choice

---
+++ script.js
@@ -0,0 +1,14 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  //Getting a random number between 1 and 3 to assign rock, paper, or scissors
+  let randomNum = Math.floor(Math.random() * (3 - 1) + 1);
+  if (randomNum === 1) {
+    return "rock";
+  } else if (randomNum === 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
